e8508bdf9ed12c531a063780df967f60f7030f86
SoftCancer <ygxzzu@163.com>
2020-02-01 02:31:36 +0800

    创建实体类模块。

---
+++ shopmallparent/shop-mall-api-entitys/shop-mall-weixin-entity/src/main/java/com/dongl/entity/AppEntity.java
@@ -0,0 +1,18 @@
+package com.dongl.entity;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @author: YaoGuangXun
+ * @date: 2020/2/1 2:09
+ * @Version: 1.0
+ */
+@Data
+public class AppEntity {
+
+    private String id;
+
+    private String name;
+
+}
