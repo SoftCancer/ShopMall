eb3256822972d321da3c676011a27828ccc4fdd2
SoftCancer <ygxzzu@163.com>
2020-02-01 00:43:06 +0800

    初次提交代码

---
+++ shopmallparent/shop-mall-basics/shop-basics-eureka/src/main/java/com/dongl/shopbasicseureka/ShopBasicsEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.dongl.shopbasicseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBasicsEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBasicsEurekaApplication.class, args);
+    }
+
+}
