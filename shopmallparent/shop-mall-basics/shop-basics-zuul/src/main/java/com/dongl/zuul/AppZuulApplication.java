670afb194ebd5bff66733d08ea9d37b6580dc8bf
SoftCancer <ygxzzu@163.com>
2020-02-01 19:22:31 +0800

    项目通过zuul 路由网关 整合 Swagger Api 接口 中级版。

---
+++ shopmallparent/shop-mall-basics/shop-basics-zuul/src/main/java/com/dongl/zuul/AppZuulApplication.java
@@ -0,0 +1,60 @@
+package com.dongl.zuul;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: 微服务网关入口
+ * @author: YaoGuangXun
+ * @date: 2020/2/1 18:35
+ * @Version: 1.0
+ */
+@EnableSwagger2Doc   // 开启Swagger功能
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class AppZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppZuulApplication.class,args);
+    }
+
+
+    /**
+     *  通过 Zuul 实现 Swagger Api 的路由转发
+     * @Author: YaoGuangXun
+     * @Date: 2020/2/1 19:04
+     **/
+    @Component
+    @Primary
+    public class DocumentationConfig implements SwaggerResourcesProvider{
+
+        @Override
+        public List<SwaggerResource> get() {
+
+            List resources = new ArrayList();
+            resources.add(swaggerResource("app-member","/app-member/v2/api-docs","2.0.0"));
+            resources.add(swaggerResource("app-weixin","/app-weixin/v2/api-docs","2.0.0"));
+            return resources;
+        }
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+}
