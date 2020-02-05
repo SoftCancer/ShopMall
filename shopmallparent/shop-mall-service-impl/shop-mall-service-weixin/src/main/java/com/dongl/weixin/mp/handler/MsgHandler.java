e1fce226fe431c9455d1d9b68d8e26f566ae8d66
SoftCancer <ygxzzu@163.com>
2020-02-05 14:42:22 +0800

    添加Redis 工具类以及常量，实现返回注册码

--- shopmallparent/shop-mall-service-impl/shop-mall-service-weixin/src/main/java/com/dongl/weixin/mp/handler/MsgHandler.java
+++ shopmallparent/shop-mall-service-impl/shop-mall-service-weixin/src/main/java/com/dongl/weixin/mp/handler/MsgHandler.java
@@ -1,5 +1,6 @@
 package com.dongl.weixin.mp.handler;
 
+import com.dongl.core.utils.RegexUtils;
 import com.dongl.weixin.mp.builder.TextBuilder;
 import com.dongl.weixin.mp.utils.JsonUtils;
 import me.chanjar.weixin.common.error.WxErrorException;
@@ -8,9 +9,11 @@ import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Random;
 
 import static me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
 
@@ -19,6 +22,13 @@ import static me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
  */
 @Component
 public class MsgHandler extends AbstractHandler {
+
+    // 模板消息
+    @Value("${dongl.weixin.template.code.message}")
+    private String templateMsg ;
+    // 默认消息
+    @Value("${dongl.weixin.default.template.code.message}")
+    private String defaultMsg ;
 
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
@@ -43,10 +53,33 @@ public class MsgHandler extends AbstractHandler {
         }
 
         //TODO 组装回复消息
-        String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
+//        String content = "收到信息内容：" + JsonUtils.toJson(wxMessage);
+
+        String msgType = wxMessage.getMsgType();
+        logger.info("公众号回复消息： "+ templateMsg + "  消息类型：" + msgType);
+        String content = "你好，请输入 ：您的手机号 ！";
+
+        String msg = wxMessage.getContent();
+        logger.info("收到用户发送的消息："+ msg);
+
+        if (RegexUtils.checkMobile(msg)){
+            int registCode =  registCode();
+            content = templateMsg.replaceAll("templateMsg",registCode+"");
+            new TextBuilder().build(content, wxMessage, weixinService);
+        }
 
         return new TextBuilder().build(content, wxMessage, weixinService);
 
+    }
+
+    /**
+     *  获取4位随机数 的获取注册码
+     * @Author: YaoGuangXun
+     * @Date: 2020/2/5 0:52
+     **/
+    private int registCode() {
+        int registCode = (int) (Math.random() * 9000 + 1000);
+        return registCode;
     }
 
 }
